398da376106cace4317adeb03f8ffc4e4faf1621
Gestor Energetico <informatica@gestor-energetico.com>
2017-08-01 15:46:37 +0200

    changes

---
+++ application/views/template/footer.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>CodeIgniter</title>
+  </head>
+  <body>
+      <h1><?php echo $title; ?></h1>
+        <em>&copy; 2015</em>
+  </body>
+</html>
