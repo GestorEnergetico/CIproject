57203447b713fc7bd2644b8bf8febdd05637e356
Gestor Energetico <informatica@gestor-energetico.com>
2017-08-02 15:26:47 +0200

    this is a f* change

--- application/controllers/pages.php
+++ application/controllers/pages.php
@@ -6,7 +6,8 @@ class Pages extends CI_Controller {
       if ( ! file_exists(APPPATH.'views/pages/'.$page.'.php'))
       {
               // Whoops, we don't have a page for that!
-              show_404();
+              // show_404();
+              echo "sddasdasd";
       }
 
       $data['title'] = ucfirst($page); // Capitalize the first letter
