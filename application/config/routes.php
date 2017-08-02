644269220af97c773473ff832759a75aa0e58120
Gestor Energetico <informatica@gestor-energetico.com>
2017-08-02 16:08:08 +0200

    def?

--- application/config/routes.php
+++ application/config/routes.php
@@ -52,5 +52,5 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 // $route['default_controller'] = 'welcome';
 // $route['404_override'] = '';
 // $route['translate_uri_dashes'] = FALSE;
-$route['default_controller'] = 'pages';
+$route['default_controller'] = 'paginas';
 // $route['(:any)'] = '$1';
