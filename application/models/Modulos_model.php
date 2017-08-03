d398e6ccc179ca98719789e80bfbc718e0939bbb
Gestor Energetico <informatica@gestor-energetico.com>
2017-08-03 15:49:30 +0200

    changes

---
+++ application/models/Modulos_model.php
@@ -0,0 +1,19 @@
+<?php
+
+class Modulos_model extends CI_Model {
+
+  public function __construct(){
+      parent::__construct();
+  }
+
+  function custom_get() {
+    $result = false;
+    if ($this->db->simple_query('SELECT * FROM modulos')){
+      // echo "Success!";
+      $result = $query->result();
+      // }else{
+      // echo "Query failed!";
+    }
+    return $result;
+  }
+}
