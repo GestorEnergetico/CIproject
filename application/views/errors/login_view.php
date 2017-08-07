94f44a28d31a4bca715f33114cc6632f3f51ff6e
Gestor Energetico <informatica@gestor-energetico.com>
2017-08-07 10:11:24 +0200

    test

--- application/views/errors/login_view.php
+++ application/views/errors/login_view.php
@@ -18,7 +18,38 @@
     <![endif]-->
   </head>
   <body>
-    <h1>Hello, world!</h1>
+
+    <!-- lOGIN -->
+    <div class="container" style="margin-top:80px;">
+
+      <div class="row">
+        <div class="col-md-4"></div>
+          <div class="col-md-4">
+            <div class="panel panel-default">
+              <div class="panel-body">
+
+                <form action="<?php echo site_url('login') ?>" method="post">
+                  <div class="form-group">
+
+                    <label for="exampleInputEmail1">Email address</label>
+                    <input type="email" class="form-control" id="exampleInputEmail1" name="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+                    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+                  </div>
+
+                 <div class="form-group">
+                   <label for="exampleInputPassword1">Password</label>
+                   <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+                 </div>
+                  <button type="submit" class="btn btn-primary">Login</button>
+                  <a href="<?php echo site_url('login') ?>" class="btn btn-link"</a>
+                 </form>
+
+              </div>
+            </div>
+          </div>
+
+      </div>
+    </div>
 
     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
     <script src="<?php echo base_url() ?>assets/js/jquery.min.js"</script>
