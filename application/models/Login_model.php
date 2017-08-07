36e77f467664e111dd41e07f93670fb7ee3c87c9
Gestor Energetico <informatica@gestor-energetico.com>
2017-08-07 10:56:09 +0200

    test

---
+++ application/models/Login_model.php
@@ -0,0 +1,22 @@
+<?php
+
+class Modulos_model extends CI_Model {
+
+  public function __construct(){
+    parent::__construct();
+  }
+  function login($username, $password) {
+    $this->db->select('id,fullname,username,password');
+    $this->db->from('user');
+    $this->db->where('username', $username);
+    $this->db->where('password', $password);
+    $this->db->limit(1);
+
+    $query = $this->db->get();
+    if ($query->num_rows()==1) {
+      return $query->result();
+    }else  {
+      return false;
+    }
+  }
+}
