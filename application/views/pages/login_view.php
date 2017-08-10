6fbc4ada152a27e3af9b12b3a6f56233fc40e874
Gestor Energetico <informatica@gestor-energetico.com>
2017-08-10 09:09:17 +0200

    login

--- application/views/pages/login_view.php
+++ application/views/pages/login_view.php
@@ -23,7 +23,7 @@
       <div class="box">
         <img style="max-width:120px;" src="img\gestor-energetico-logo.png" alt="Logo Gestor Energético">
         <div class="login">
-        <label><i class="fa fa-user" aria-hidden="true"></i> Username<input type="text" placeholder="Username" name="username" value=""></label>
+        <label><i class="fa fa-user" aria-hidden="true"></i> Username<input type="text" placeholder="Username" name="username" value=""></label><br>
         <label><i class="fa fa-lock" aria-hidden="true"></i> Password<input type="password" placeholder="Password" name="password" value=""></label>
         <button type="button" name="button">Log in</button>
       </div>
