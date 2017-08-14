d8b6bb80adacb65925ede714ffcf5c4832351731
Gestor Energetico <informatica@gestor-energetico.com>
2017-08-14 10:42:45 +0200

    register

--- application/controllers/Users.php
+++ application/controllers/Users.php
@@ -5,9 +5,9 @@ class User extends CI_Controller {
 public function __construct(){
 
         parent::__construct();
-  			$this->load->helper('url');
-  	 		$this->load->model('user_model');
-        $this->load->library('session');
+  			// $this->load->helper('url');
+        // his->load->model('user_model');
+        // $this->load->library('session');
 
 }
 
