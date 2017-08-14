39ab16539c58c8d0cb2c442d491cb21c62f59cc2
Gestor Energetico <informatica@gestor-energetico.com>
2017-08-14 10:24:41 +0200

    register

--- application/config/config.php
+++ application/config/config.php
@@ -35,7 +35,7 @@ $config['base_url'] = 'http://devadmin.gestor-energetico.com/';
 | variable so that it is blank.
 |
 */
-$config['index_page'] = 'index.php';
+$config['index_page'] = '';
 
 /*
 |--------------------------------------------------------------------------
