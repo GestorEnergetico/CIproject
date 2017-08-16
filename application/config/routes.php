c5314de88fa9db82cb0a8bc4b054912c64e93be4
Gestor Energetico <informatica@gestor-energetico.com>
2017-08-16 11:34:19 +0200

    register

--- application/config/routes.php
+++ application/config/routes.php
@@ -52,5 +52,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 // $route['default_controller'] = 'welcome';
 // $route['404_override'] = '';
 // $route['translate_uri_dashes'] = FALSE;
-$route['default_controller'] = 'user';
 // $route['(:any)'] = '$1';
+
+
+$route['default_controller'] = 'user';
+$route['user/login'] = 'login';
