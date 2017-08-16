2954615ba0fdecb9b5e7ac7a89fcba423ffeb540
Gestor Energetico <informatica@gestor-energetico.com>
2017-08-16 09:08:07 +0200

    register

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -29,7 +29,7 @@ class User extends CI_Controller {
         if($email_check){
           $this->user_model->register_user($user);
           $this->session->set_flashdata('success_msg', 'Registered successfully.Now login to your account.');
-          redirect('user/login_view');
+          redirect('pages/login_view2');
 
         }
         else{
