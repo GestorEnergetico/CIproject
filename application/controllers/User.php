3b22997faa6de1eec585bce0322fc4449a4776c4
Gestor Energetico <informatica@gestor-energetico.com>
2017-08-16 11:02:58 +0200

    register

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -48,17 +48,19 @@ class User extends CI_Controller {
     );
 
     $data=$this->user_model->login_user($user_login['user_email'],$user_login['user_password']);
+
     if($data){
       $this->session->set_userdata('user_id',$data['user_id']);
       $this->session->set_userdata('user_email',$data['user_email']);
       $this->session->set_userdata('user_name',$data['user_name']);
       $this->session->set_userdata('user_age',$data['user_age']);
       $this->session->set_userdata('user_mobile',$data['user_mobile']);
-
-      redirect('user/user_profile');
+      // view
+      $this->load->view('pages/User_profile_view.php');
     }else{
       $this->session->set_flashdata('error_msg', 'Error occured,Try again.');
-      redirect("user/login_view");
+      // view
+      $this->load->view("pages/Login_view2.php");
     }
   }
 
