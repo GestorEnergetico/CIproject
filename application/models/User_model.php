da453da70d170a23fa3211b3ff907550c081de42
Gestor Energetico <informatica@gestor-energetico.com>
2017-08-16 11:13:37 +0200

    register

--- application/models/User_model.php
+++ application/models/User_model.php
@@ -2,42 +2,40 @@
 class User_model extends CI_model{
 
   public function __construct(){
-      parent::__construct();
+    parent::__construct();
   }
 
   public function register_user($user){
-
+    
     $this->db->insert('user', $user);
   }
 
   public function login_user($email,$pass){
 
-  $this->db->select('*');
-  $this->db->from('user');
-  $this->db->where('user_email',$email);
-  $this->db->where('user_password',$pass);
+    $this->db->select('*');
+    $this->db->from('user');
+    $this->db->where('user_email',$email);
+    $this->db->where('user_password',$pass);
 
-  if($query=$this->db->get())
-  {
+    if($query=$this->db->get()){
       return $query->row_array();
-  }
-  else{
-    return false;
+    }else{
+      return false;
     }
   }
   public function email_check($email){
 
-  $this->db->select('*');
-  $this->db->from('user');
-  $this->db->where('user_email',$email);
-  $query=$this->db->get();
+    $this->db->select('*');
+    $this->db->from('user');
+    $this->db->where('user_email',$email);
+    $query=$this->db->get();
 
-  if($query->num_rows()>0){
-    return false;
-  }else{
-    return true;
+    if($query->num_rows()>0){
+      return false;
+    }else{
+      return true;
+    }
+
   }
-
-}
 
 }
