f4719a5f2d72cb8d3e733e379244f93b7088e57b
Gestor Energetico <informatica@gestor-energetico.com>
2017-08-17 10:27:28 +0200

    register refactor

--- application/controllers/Alumnos.php
+++ application/controllers/Alumnos.php
@@ -4,6 +4,7 @@ class Alumnos extends CI_Controller{
 
   function __construct()
   {
+    parent::__construct();
     $this->load->helper('url');
     $this->load->model('alumnos_model');
     $this->load->library('session');
