49f82edcdb4a946d8f7dc6343d89bdc703624f5d
Gestor Energetico <informatica@gestor-energetico.com>
2017-08-23 18:32:30 +0200

    template implementation

--- application/views/pages/admin/Alumnos_list.php
+++ application/views/pages/admin/Alumnos_list.php
@@ -2,7 +2,7 @@
 <?php
 $out = '';
 foreach ($result as $key => $value) {
-  $out .= $key . " - " . $value["email_users"] . "<br/>";
+  $out .= $key . " - " . $value["email_user"] . "<br/>";
 }
 echo $out;
 ?>
