3f49f9aebfea81592a844b3844f596b04a698e22
Gestor Energetico <informatica@gestor-energetico.com>
2017-08-24 18:17:02 +0200

    implementing login and template

--- application/config/routes.php
+++ application/config/routes.php
@@ -57,7 +57,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 /*    Basic routes    */
 $route['default_controller'] = 'user';
 /*    /Basic routes    */
-$route['user/user_logout'] = '/logout';
+// $route['user/user_logout'] = '/logout';
 
 
 /*    Test routes    */
