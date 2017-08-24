0492a75735bf214c5bf072a0fc2dee024086d42e
Gestor Energetico <informatica@gestor-energetico.com>
2017-08-24 17:39:33 +0200

    implementing login and template

--- application/models/User_model.php
+++ application/models/User_model.php
@@ -13,9 +13,9 @@ class User_model extends CI_model{
   public function login_user($email,$pass){
 
     $this->db->select('*');
-    $this->db->from('user');
-    $this->db->where('user_email',$email);
-    $this->db->where('user_password',$pass);
+    $this->db->from('users');
+    $this->db->where('email_user',$email);
+    $this->db->where('password_user',$pass);
 
     if($query=$this->db->get()){
       return $query->row_array();
@@ -26,8 +26,8 @@ class User_model extends CI_model{
   public function email_check($email){
 
     $this->db->select('*');
-    $this->db->from('user');
-    $this->db->where('user_email',$email);
+    $this->db->from('users');
+    $this->db->where('email_user',$email);
     $query=$this->db->get();
 
     if($query->num_rows()>0){
