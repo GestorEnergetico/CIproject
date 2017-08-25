f171ebf431e2dcd8323e904c362eefe17422229e
Gestor Energetico <informatica@gestor-energetico.com>
2017-08-25 18:07:19 +0200

    logout

--- application/views/pages/admin/Profesores_list.php
+++ application/views/pages/admin/Profesores_list.php
@@ -1,5 +1,4 @@
 <?php
-print_r($result);
 $out = '';
 foreach ($result as $key => $value) {
   $out .= "<a href='/profesores/'".$value["id_user"] . "> ".$value["id_user"] ."  - " . $value["email_user"] . "</a><br/>";
