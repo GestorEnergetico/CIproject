bb01962297ac18245ca195ef65599fa504031918
Gestor Energetico <informatica@gestor-energetico.com>
2017-08-28 16:12:25 +0200

    aulas

--- application/views/pages/admin/Aulas_list.php
+++ application/views/pages/admin/Aulas_list.php
@@ -3,7 +3,7 @@
 echo "<ol>";
 foreach ($result as $course) {
   echo "<li>";
-  echo "<a href='/aulas/.$course["id_aula"]'>".$course["name_module"]."</a>";
+  echo "<a href='/aulas/.$course["id_aula"]'>.$course["name_module"]."</a>";
   echo "</li>";
 }
 echo "</ol>";
