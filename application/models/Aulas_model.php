e6f1b3c75090ca673f00dec5b1912aa2ba189eb4
Gestor Energetico <informatica@gestor-energetico.com>
2017-08-28 18:22:21 +0200

    aulas

--- application/models/Aulas_model.php
+++ application/models/Aulas_model.php
@@ -13,14 +13,13 @@ class Aulas_model extends CI_model{
     return $result = $query->result_array();
 }
 
-public function get_id($id){
-  $this->db->select('id_course, name_module');
-  $this->db->from('courses');
-  $this->db->join('modules', 'courses.fk_module = modules.id_module', 'left');
-  $query = $this->db->get();
-  return $result = $query->result_array();
-}
-
-
+  public function get_id($id){
+    $this->db->select('id_course, name_module');
+    $this->db->from('courses');
+    $this->db->join('modules', 'courses.fk_module = modules.id_module', 'left');
+    $this->db->where('courses.id_course', $id);
+    $query = $this->db->get();
+    return $result = $query->result_array();
+  }
 
 }
