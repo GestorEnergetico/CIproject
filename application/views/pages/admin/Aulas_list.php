d10708d6f466d81dfbf698a73cab92e3aa9741fb
Gestor Energetico <informatica@gestor-energetico.com>
2017-08-28 16:06:25 +0200

    aulas

--- application/views/pages/admin/Aulas_list.php
+++ application/views/pages/admin/Aulas_list.php
@@ -1,26 +1,10 @@
 
 <?php
-// $out = '';
-
-print_r($result);
 echo "<ol>";
 foreach ($result as $course) {
   echo "<li>";
-  echo "<a href=''>".$course["name_module"]."</a>";
+  echo "<a href='/aulas/id_aula'>".$course["name_module"]."</a>";
   echo "</li>";
 }
 echo "</ol>";
-// foreach ($result as $key => $value) {
-//
-//   $out .= $key . " - " . $value["id_aula"] . "<br/>";
-// }
-// echo $out;
-
 ?>
-
-
-<ol>
-  <li>
-    <a href="#"></a>
-  </li>
-</ol>
