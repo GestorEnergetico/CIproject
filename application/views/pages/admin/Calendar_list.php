f47b368ddab8a53bbc7de91e147423d27b8be63a
Gestor Energetico <informatica@gestor-energetico.com>
2017-08-29 14:50:35 +0200

    calendars

--- application/views/pages/admin/Calendar_list.php
+++ application/views/pages/admin/Calendar_list.php
@@ -3,7 +3,7 @@
 echo "<ol>";
 foreach ($result as $course) {
   echo "<li>";
-  echo "<a href='/calendars/". $course["id_calendar"] ."'>" . $course["name_calendar"] . "</a>";
+  echo "<a href='/calendar/". $course["id_calendar"] ."'>" . $course["name_calendar"] . "</a>";
   echo "</li>";
 }
 echo "</ol>";
