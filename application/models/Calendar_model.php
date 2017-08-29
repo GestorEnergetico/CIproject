8ab23a79a02696aa431087ae8da762c41f75e6cf
Gestor Energetico <informatica@gestor-energetico.com>
2017-08-29 14:54:08 +0200

    calendars

--- application/models/Calendar_model.php
+++ application/models/Calendar_model.php
@@ -12,5 +12,14 @@ class Calendar_model extends CI_model{
     $query = $this->db->get();
     return $result = $query->result_array();
 }
+public function get_id($id){
+  $this->db->select('id_calendar, name_calendar');
+  $this->db->from('calendars');
+  // $this->db->join('modules', 'courses.fk_module = modules.id_module', 'left');
+  // $this->db->where('courses.id_course', $id);
+  $query = $this->db->get();
+  return $result = $query->result_array();
+}
+
 
 }
