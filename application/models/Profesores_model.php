b2513cf2741226437b40488e5f0fceca5db02549
Gestor Energetico <informatica@gestor-energetico.com>
2017-08-29 17:04:42 +0200

    prof

--- application/models/Profesores_model.php
+++ application/models/Profesores_model.php
@@ -16,12 +16,12 @@ class Profesores_model extends CI_model{
   public function get_id($id){
     $this->db->select('name_user, email_user, name_role');
     $this->db->from('users_data');
-    $this->db->join('users', 'users.id_user = users_data.fk_user');
-    $this->db->join('roles', 'roles.id_role = users.fk_role', 'left');
-    $this->db->where('fk_user', $id);
+    // $this->db->join('users', 'users.id_user = users_data.fk_user');
+    // $this->db->join('roles', 'roles.id_role = users.fk_role', 'left');
+    // $this->db->where('fk_user', $id);
     $query = $this->db->get();
     return $result = $query->result_array();
   }
 
-  
+
 }
