253cae33ba8d32d7bb880ada4440e3d41d07d8db
fstascon <tascon17@gmail.com>
2017-09-12 18:24:11 +0200

    updating login page

--- application/views/pages/Login.php
+++ application/views/pages/Login.php
@@ -27,18 +27,18 @@
     <?php
   }
   ?>
-  <form  action="<?php echo base_url('user/login'); ?>" method="post">
-    <div class="content_login flex">
+  <div class="content_login flex">
+    <form  action="<?php echo base_url('user/login'); ?>" method="post">
       <div class="box">
-        <img style="max-width:120px;" src="img\gestor-energetico-logo.png" alt="Logo Gestor Energético">
+        <img style="max-width:120px;" src="assets/img/gestor-energetico-logo.png" alt="Logo Gestor Energético">
         <div class="login">
           <label><i class="fa fa-user" aria-hidden="true"></i> <input type="text" placeholder="Username" name="user_email" value=""></label>
           <label><i class="fa fa-lock" aria-hidden="true"></i> <input type="password" placeholder="Password" name="user_password" value=""></label>
           <button type="submit" name="button">Log in</button>
         </div>
       </div>
-    </div>
-  </form>
+    </form>
+  </div>
 </body>
 </html>
 <!-- based in: https://cdn.elegantthemes.com/blog/wp-content/uploads/2016/06/divi-login-page-5.jpg -->
