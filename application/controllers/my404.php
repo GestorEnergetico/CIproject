654af1a585795a63c4ce801fbf56d85df2adc101
fstascon <tascon17@gmail.com>
2017-09-13 16:59:02 +0200

    custom 404

--- application/controllers/my404.php
+++ application/controllers/my404.php
@@ -8,7 +8,7 @@ class My404 extends CI_Controller
 
   public function index(){
     if($this->session->has_userdata("email_user")){
-      $this->data['session'] = ($this->session->userdata);
+      $this->data['session'] = ($this->session->userdata());
       $this->output->set_status_header('404');
       $this->data['middle'] = 'pages/error_404';
       $this->load->view('template',$this->data);
