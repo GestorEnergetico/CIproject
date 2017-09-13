eb0a6177eaa6efd69b4956c8617de7117f2f929a
fstascon <tascon17@gmail.com>
2017-09-13 17:34:08 +0200

    custom 404

--- application/controllers/my404.php
+++ application/controllers/my404.php
@@ -9,6 +9,24 @@ class My404 extends CI_Controller
   public function index(){
     if($this->session->has_userdata("email_user")){
       $this->output->set_status_header('404');
+      switch ($this->session->role) {
+        case 1:
+        $tmp = "alumno";
+        break;
+        case 2:
+        $tmp = "profesor";
+        break;
+        case 3:
+        $tmp = "cordinador";
+        break;
+        case 10:
+        $tmp = "administrador";
+        break;
+        default:
+        $tmp = "alumno";
+        break;
+      }
+      $this->data["role"] = $tmp;
       $this->load->view('pages/error_404',$this->data);
     }
   }
--- application/views/pages/error_404.php
+++ application/views/pages/error_404.php
@@ -1,2 +1,3 @@
 <?php print_r($this->session->userdata()); ?>
 <p>PAGE NOT FOUND</p>
+<?php echo $role; ?>
