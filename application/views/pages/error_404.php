d99ff58add0bcb8db82a9a5e9ac30266d9009da4
fstascon <tascon17@gmail.com>
2017-09-13 17:00:35 +0200

    custom 404

--- application/views/pages/error_404.php
+++ application/views/pages/error_404.php
@@ -1,14 +1,4 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="utf-8">
-    <title>404</title>
-  </head>
-  <body>
 <?php
 print_r($session);
- ?>
-    <p>PAGE NOT FOUND</p>
-
-  </body>
-</html>
+?>
+<p>PAGE NOT FOUND</p>
