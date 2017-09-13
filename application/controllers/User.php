23d20a71ad0bd5df77383421bafd90398b02dedf
fstascon <tascon17@gmail.com>
2017-09-13 15:55:59 +0200

    redirect controllers

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -8,19 +8,14 @@ class User extends CI_Controller{
     $this->load->helper('url');
     $this->load->model('user_model');
     $this->load->library('session');
-    if(!$this->session->has_userdata("email_user")){
-      if($this->uri->uri_string() != "login"){
-        $this->session->set_flashdata('refer', $this->uri->uri_string());
-        redirect('/login');
-      }else{
-        $this->load->view("pages/Login.php");
-      }
-    }
   }
   public function index(){
     if($this->session->has_userdata("email_user")){
       $this->data['middle'] = 'pages/Dashboard.php';
       $this->load->view('template',$this->data);
+    }else {
+      $this->session->set_flashdata('refer', $this->uri->uri_string());
+      $this->load->view("pages/Login.php");
     }
   }
   public function create(){
