5ceed381cfd0e19c82c8d0f58288fb87563913b8
fstascon <tascon17@gmail.com>
2017-09-14 15:57:04 +0200

    custom calendar

--- application/views/pages/admin/Single_calendar.php
+++ application/views/pages/admin/Single_calendar.php
@@ -1,19 +1,39 @@
 <?php
 
-$out='';
-// print_r($years);
+// by default year
+$anio='';
 foreach ($years as $year) {
-  $out .= "<li>".$year["year_edition"]."</li>";
+  $anio .= "<li>".$year["year_edition"]."</li>";
 }
+
+
 
 ?>
 <div class="row single_calendar ">
-  <div class="col-sm-4">
+  <div class="col-sm-3">
     <ul class="anio">
       <li>
         <h3>Años</h3>
       </li>
-      <?php echo $out; ?>
+      <?php echo $anio; ?>
+    </ul>
+  </div>
+
+  <div class="col-sm-4">
+    <ul class="edition">
+      <li>
+        <h3>Edicion</h3>
+      </li>
+      <?php echo $anio; ?>
+    </ul>
+  </div>
+
+  <div class="col-sm-4">
+    <ul class="course">
+      <li>
+        <h3>Aula</h3>
+      </li>
+      <?php echo $anio; ?>
     </ul>
   </div>
 </div>
