dbd7bd9f974b348fd6a80373c56ae854f9c68779
fstascon <tascon17@gmail.com>
2017-09-15 19:05:35 +0200

    up-to-date

--- application/views/pages/dashboard.php
+++ application/views/pages/dashboard.php
@@ -1,3 +1,7 @@
-<p>Lasts Courses CI model asking for date create_AT</p>
-<p>Lasts Users CI Model asking for date create_AT</p>
-<p>Main Calendar with fullcalendar.js</p>
+<p>Some widgets</p>
+<p>Some widgets</p>
+<p>Some widgets</p>
+<p>Some widgets</p>
+<p>Some widgets</p>
+<p>Some widgets</p>
+<p>Some widgets</p>
